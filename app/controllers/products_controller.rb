568c9202f222c5e727a99f42b5247894d710b065
Joe Bernardi <joebernardi@hotmail.com>
2016-12-01 23:04:08 -0600

    add erb method for categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,14 @@ class ProductsController < ApplicationController
 	def index
 
 		@products  = Product.all
-
+		@categories = Category.all
 	end
 
+	def show
+
+    @category = Category.find(params[:id])
+
+
+	end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ApplicationRecord
-	belongs_to :category, :foreign_key => 'category_id'
+	belongs_to :category, :foreign_key => 'category_id', dependent: :destroy
 end
