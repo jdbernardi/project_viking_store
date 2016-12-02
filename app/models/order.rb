540ea5d2b0f03e5c603b98dc132af70470e0712f
Joe Bernardi <joebernardi@hotmail.com>
2016-12-02 14:57:09 -0600

    relations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
 	belongs_to :user
+	has_one :order_content
 	belongs_to :address, :foreign_key => "shipping_id"
 	belongs_to :address, :foreign_key => "billing_id"
 	has_many :products
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
 	belongs_to :category, :foreign_key => 'category_id', dependent: :destroy
+	has_many :orders
 end
