226018c437afffb51b1512992d2b7d5a94f0078f
Joe Bernardi <joebernardi@hotmail.com>
2016-12-02 22:49:27 -0600

    products to add price cat

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
 
+    include ProductsHelper
+
 	def index
 
 		@products  = Product.all
@@ -13,6 +15,21 @@ class ProductsController < ApplicationController
       @product = Product.new
 
 	end
+
+    def create
+
+        @product = Product.new( products_params )
+
+        if @product.save
+            flash.notice = "Product #{@product.name} Created!"
+            redirect_to products_path
+        else
+            flash.notice = errors
+            redirect_to new_product_path
+        end
+
+    end
+
 
 
 	def show
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,15 @@
 module ProductsHelper
+
+	def products_params
+		params.require( :product ).permit(:name, :description )
+	end
+
+	def errors
+
+			flash.notice = "#{@product.errors.full_messages.join}"
+
+
+	end
+
+
 end
