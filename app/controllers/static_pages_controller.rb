ebe74c2c23440553d95cad0db8c1f3b3884c7f98
Joe Bernardi <joebernardi@hotmail.com>
2016-12-06 14:20:17 -0600

    fix price method in static pgs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
 
+	include StaticPagesHelper
 
 	def index
 
@@ -8,12 +9,7 @@ class StaticPagesController < ApplicationController
 		@total_products = Product.all
 		@order_contents = OrderContent.all
 
-		array = []
-
-		@order_contents.each{ |x| array << x.revenue }
-
-    @revenue = array.inject(0) { |r,e| r+e }
-
+		@revenue = get_revenue
 
     @user_7_days = User.where(:created_at => (Time.now.midnight - 7.days)..Time.now.midnight).count
     @order_7_days = Order.where(:created_at => (Time.now.midnight - 7.days)..Time.now.midnight).count
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,8 +1,17 @@
 module StaticPagesHelper
 
 
-	def seven_days
+	def get_revenue
 
+		arr = []
+
+		@order_contents.each do | o |
+
+			arr << ( o.quantity * o.product.price )
+
+		end
+
+		return arr.inject( 0 ) { | r, e | r + e }
 
 	end
 
