1f75667745d3c60df2b378c1fb7ea27750594354
Joe Bernardi <joebernardi@hotmail.com>
2016-12-07 13:26:12 -0600

    nested attributes for user addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 	has_many :orders
 	has_many :addresses
+	accepts_nested_attributes_for :addresses
+
 	has_many :credit_cards
 
 	validates :first_name, length: { in: 1..64 }, presence: true
