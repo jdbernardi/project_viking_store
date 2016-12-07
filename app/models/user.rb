399434f38d8d93a8bd3f2e4f95f40f357caad7f7
Joe Bernardi <joebernardi@hotmail.com>
2016-12-06 22:21:12 -0600

    validations for names and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
 	has_many :orders
 	has_many :addresses
 	has_many :credit_cards
+
+	validates :first_name, length: { in: 1..64 }, presence: true
+
+	validates :last_name, length: { in: 1..64 }, presence: true
+
+	validates :email, length: { in: 1..64 }, presence: true
+
 end
