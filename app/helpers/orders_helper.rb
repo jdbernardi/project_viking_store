fd4531c959d1e24d2b6ad52876776c516fccab1f
Joe Bernardi <joebernardi@hotmail.com>
2016-12-14 22:38:51 -0600

    display id's, address, revenue

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,19 @@
 module OrdersHelper
+
+	def revenue( order )
+
+		arr = []
+
+		order.order_contents.each do |o|
+
+			arr << o.revenue
+
+		end
+
+		return arr.inject( 0 ) { | r, e | r + e }
+
+	end
+
 end
+
+
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ApplicationRecord
 	belongs_to :city
 	belongs_to :state
+	has_many :orders
 
 	belongs_to :user
 
