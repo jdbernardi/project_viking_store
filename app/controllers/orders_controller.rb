3c455a90d5601d5a6b96a4692d55416da1e1a1e5
Joe Bernardi <joebernardi@hotmail.com>
2016-12-15 09:21:57 -0600

    correct links index param

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,35 @@ class OrdersController < ApplicationController
 
 	def index
 
-		@orders = Order.all.limit(40)
+		if params[:user]
+
+			@orders = Order.where( user_id: params[:user] ).limit(100)
+			@user = User.find( params[:user] )
+
+		else
+
+			@orders = Order.all.limit(40)
+
+		end
+
+	end
+
+
+	def show
+
+		if User.exists?(params[:id])
+
+			redirect_to orders_path( :user => params[:id] )
+
+		elsif Order.exists?(params[:id])
+
+			@order = Order.find( params[:id])
+
+		else
+
+			redirect_to orders_path
+
+		end
 
 	end
 
